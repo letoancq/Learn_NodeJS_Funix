284cc19dcbdf77d5ab109c842c4d4bd6ccec44cd
ToanFX15903 <vtv5hdcq@gmail.com>
2022-08-30 22:47:24 +0700

    add

--- Documents/Funix/NodeJS/Learn/index.js
+++ Documents/Funix/NodeJS/Learn/index.js
@@ -1,11 +1,41 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const http = require("http");
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text name="messega"><button type="submit">Submit</button></form></body'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node.js server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+server.listen(3000);
