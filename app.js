94fbcc06980c968fc93a3599b49988e51e0995cc
letoancq <103833082+letoancq@users.noreply.github.com>
2022-08-31 00:52:09 +0700

    Lab2.5

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/", (req, res, next) => {
-  console.log("this always runs!");
   next();
 });
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the other middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit" >Add Product</button></form>'
+  );
+});
 
-  res.send("<h1>The add Product page</h1>");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the other middleware");
   res.send("<h1>Hello from Express.Js</h1>");
 });
 
