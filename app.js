20e0f54f5a849491e38a15c5d9479d198be3dd72
letoancq <103833082+letoancq@users.noreply.github.com>
2022-09-01 23:13:08 +0700

    Page 404

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+var morgan = require("morgan");
 
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(morgan("combined"));
 
 const admonRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -12,4 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(admonRouter);
 app.use(shopRouter);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found </h1>");
+});
 app.listen(3000);
