2375ef8ad09a0bb15a93751c94e63b0d198545a2
letoancq <103833082+letoancq@users.noreply.github.com>
2022-09-02 01:12:56 +0700

    Lab2.14
    Cung cấp tệp tĩnh

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRouter);
 app.use(shopRouter);
