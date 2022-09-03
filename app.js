70bfd7348f1b8b26220b8610b6cec4b1e74433a5
letoancq <103833082+letoancq@users.noreply.github.com>
2022-09-03 21:17:57 +0700

    Lab3.2
    Cài đặt & triển khai Pug engine

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const path = require("path");
 
 const express = require("express");
-var morgan = require("morgan");
-
 const bodyParser = require("body-parser");
+// var morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("combined"));
+app.set("view engine", "pug");
+app.set("views", "views");
+
+// app.use(morgan("combined"));
 
 const adminData = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -19,6 +21,6 @@ app.use("/admin", adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "./", "veiws", "404.html"));
+  res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
 });
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,14 @@
 const path = require("path");
 
 const express = require("express");
-const rootDir = require("../utils/path");
 
+const rootDir = require("../utils/path");
 const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("shop.js :", adminData.products);
-  res.sendFile(path.join(rootDir, "veiws", "shop.html"));
+  res.render("shop");
 });
 
 module.exports = router;
