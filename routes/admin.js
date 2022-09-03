115fc05ddbaf53a7e73e756a9b4d051376731bd7
letoancq <103833082+letoancq@users.noreply.github.com>
2022-09-03 23:41:34 +0700

    Lab3.3
    Xuất nội dung động

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "veiws", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // /admin/add-product => POST
