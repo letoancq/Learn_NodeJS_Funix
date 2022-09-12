2d983949f5faef6f432282d97f2059fee88e8797
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-12 23:32:30 +0700

    lab 3.9
    Làm việc với EJS

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.engine(
     extname: 'hbs'
   })
 );
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
