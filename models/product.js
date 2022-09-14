49df2f864eff0688137091a200472ed280ae68d9
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-14 15:17:24 +0700

    Lab 4.6
    Cấu trúc lại code lưu trữ tệp

--- models/product.js
+++ models/product.js
@@ -1,44 +1,39 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProducrsFromFile = (cb) => {
+
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-
+    getProducrsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
+
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-
-      cb(JSON.parse(fileContent));
-    });
+    getProducrsFromFile(cb);
   }
 };
