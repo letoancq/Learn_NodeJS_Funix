6ec704512d219fe7e48d6d77b145c1c521d8a5d5
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-14 15:24:48 +0700

    Lab4.8

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
-const app = express();
 
 const errorController = require('./controllers/error');
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
+const app = express();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
--- models/product.js
+++ models/product.js
@@ -1,17 +1,16 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
+  'data',
+  'products.json'
 );
 
-const getProducrsFromFile = (cb) => {
-
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
@@ -19,21 +18,24 @@ const getProducrsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProducrsFromFile((products) => {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
-
   }
 
   static fetchAll(cb) {
-    getProducrsFromFile(cb);
+    getProductsFromFile(cb);
   }
 };
