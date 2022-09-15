2d9b39b0c802d17801dd9736be038a233d771837
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-16 01:32:31 +0700

    Lab5.2
    SQL cơ bản & tạo bảng

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./util/database');
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -12,6 +14,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products').then().catch();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
