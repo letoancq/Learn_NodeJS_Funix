92af742949164eb09e1800b544084108751193ff
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-20 01:16:25 +0700

    Lab5.13 to LAb5.20

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 const app = express();
 
@@ -13,23 +15,47 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const { log } = require("console");
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
   .sync()
   .then((result) => {
-    console.log(result);
+    {
+      return User.findById(1);
+      // console.log(result);
+    }
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Toản", email: "test@node.com" });
+    }
+
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
     console.log(err);
   });
-
