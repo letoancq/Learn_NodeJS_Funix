ab61ef2736c40663d08e09de73bfb19bf93f9972
ToanFX15903 <vtv5hdcq@gmail.com>
2022-09-23 23:29:37 +0700

    lab7.9

--- models/user.js
+++ models/user.js
@@ -14,7 +14,11 @@ const userSchema = new Schema({
   cart: {
     itmes: [
       {
-        productId: { type: Schema.Types.ObjectId , required: true},
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
         quantity: { type: Number, required: true },
       },
     ],
