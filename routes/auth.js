882053dcb908db15a83db45b7c6c75c95132da98
ToanFX15903 <vtv5hdcq@gmail.com>
2022-11-05 00:17:44 +0700

    Lab 9.6: Thêm kiểm tra dữ liệu không đồng bộ

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check, body } = require("express-validator/check");
 
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 
 const router = express.Router();
 
@@ -18,14 +19,22 @@ router.post(
       .isEmail()
       .withMessage("Please enter a valid email")
       .custom((value, { req }) => {
-        if (value === "test@test.com") {
-          throw new Error("This email address if forbidden.");
-        }
-        return true;
+        // if (value === "test@test.com") {
+        //   throw new Error("This email address if forbidden.");
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              "Email exists already, please pick a different one."
+            );
+          }
+        });
       }),
     body("password", "Please enter a password with only 5 to 8 characters.")
       .isLength({ min: 5, max: 8 })
       .isAlphanumeric(),
+
     body("confirmPassword").custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error("Password have to match!");
