8968f96d1c2060bf44304ddcc970c331fc944475
ToanFX15903 <vtv5hdcq@gmail.com>
2022-11-04 23:50:00 +0700

    Lab 9.3: Trình xác thực tích hợp & tùy chỉnh

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,15 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email").isEmail().withMessage("Please enter a valid email"),
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email address if forbidden.");
+      }
+      return true;
+    }),
   authController.postSignup
 );
 
