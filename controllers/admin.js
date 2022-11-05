6be674b2b15f4097af079ae8e98e40631b404754
ToanFX15903 <vtv5hdcq@gmail.com>
2022-11-05 23:37:02 +0700

    Lab 9.15: Trả lại các trang lỗi

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const Product = require("../models/product");
 
 const { validationResult } = require("express-validator/check");
@@ -24,7 +26,7 @@ exports.postAddProduct = (req, res, next) => {
     console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
       pageTitle: "Add Product",
-      path: "/admin/edit-product",
+      path: "/admin/add-product",
       editing: false,
       hasError: true,
       product: {
@@ -39,6 +41,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
+    _id: new mongoose.Types.ObjectId("632dd6dd2c0e32f1cbfd1974"),
     title: title,
     price: price,
     description: description,
@@ -53,7 +56,21 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect("/admin/products");
     })
     .catch((err) => {
-      console.log(err);
+      // return res.status(500).render("admin/edit-product", {
+      //   pageTitle: "Add Product",
+      //   path: "/admin/add-product",
+      //   editing: false,
+      //   hasError: true,
+      //   product: {
+      //     title: title,
+      //     imageUrl: imageUrl,
+      //     price: price,
+      //     description: description,
+      //   },
+      //   errorMessage: "Database operation failed,please try again",
+      //   validationErrors: [],
+      // });
+      res.redirect("/500");
     });
 };
 
