173a0924bb943d0803ea0043e2e01f6c2de3d86c
ToanFX15903 <vtv5hdcq@gmail.com>
2022-12-15 12:32:44 +0700

    Lab 10.14: Tạo tệp .pdf với dữ liệu đặt hàng

--- controllers/shop.js
+++ controllers/shop.js
@@ -176,8 +176,28 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text("Hello world!");
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
 
+      pdfDoc.text("----------------------");
+
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice = totalPrice + prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
+      });
+      pdfDoc.text("--------------");
+      pdfDoc.fontSize(20).text("Total: " + "$" + totalPrice);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
